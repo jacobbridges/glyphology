5c80fef1dc891fe9cb8bb3ff995471181916a75f
Jacob Bridges <me@jacobbridges.dev>
2023-12-17 00:33:24 -0600

    initial commit

---
+++ src/main/java/nivix/glyphology/GlyphologyClient.java
@@ -0,0 +1,10 @@
+package nivix.glyphology;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlyphologyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
